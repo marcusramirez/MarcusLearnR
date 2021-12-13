54a41d2ce2fdb1ed2cc572316177b0efc31e3526
marcusramirez <marcra@sas.upenn.edu>
2021-12-13 00:35:30 -0500

    update

---
+++ Untitled.R
@@ -0,0 +1,10 @@
+dat1 <- read_csv("~/Downloads/dat.nsduh.small.csv")
+summary(dat1)
+dim(dat1)
+hist(dat1$AGE2, main="Histogram of AGE2", xlab="Years", ylab="Frequency")
+plot (years, frequency)
+plot.xy(dat1$AGE2)
+# histogram ---------------------------------------------------------------
+
+
+
